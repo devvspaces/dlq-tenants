55557b6235e6a340b5c0a7537f6649f5a35f0d7d
Damilare Tolulope <damilaretolulope1@gmail.com>
2024-05-10 23:08:26 +0100

    chore: unprotected protected routes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,37 +2,34 @@
 
 import Header from "@/components/Header";
 import { LOGIN } from "@/constants/path";
-import React, { useEffect } from "react";
+import React from "react";
 import { useToast } from "@/components/ui/use-toast";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { toast } = useToast();
 
-  // useEffect(() => {
-
   // check if token is in local storage
-  if (typeof window !== "undefined") {
-    const token = window.localStorage.getItem("accessToken");
+  // if (typeof window !== "undefined") {
+  //   const token = window.localStorage.getItem("accessToken");
 
-    if (!token) {
-      toast({
-        title: "Please Log In",
-        description: "No session",
-        variant: "destructive",
-      });
+  //   if (!token) {
+  //     toast({
+  //       title: "Please Log In",
+  //       description: "No session",
+  //       variant: "destructive",
+  //     });
 
-      window.location.href = LOGIN;
-      return;
-    }
-    // }, []);
+  //     window.location.href = LOGIN;
+  //     return;
+  //   }
 
-    return (
-      <div>
-        <Header />
-        <div className="mt-20 px-20">{children}</div>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <div className="mt-20 px-20">{children}</div>
+    </div>
+  );
 };
+// };
 
 export default Layout;
