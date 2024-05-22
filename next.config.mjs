95df7b2d3913375cd883767eb0a91c253e71a5a2
Damilare Tolulope <damilaretolulope1@gmail.com>
2024-05-22 13:43:18 +0100

    chore: update next config for SSG

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  output: "export",
+};
 
 export default nextConfig;
