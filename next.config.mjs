1a8f3c1a278b34e90932b90ce810cba2ab368468
Ayanwola Ayomide <77179231+devvspaces@users.noreply.github.com>
2024-05-29 20:00:52 +0100

    Another

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
+          },
+        ],
+      },
+    ];
+  }
 };
 
 export default nextConfig;
