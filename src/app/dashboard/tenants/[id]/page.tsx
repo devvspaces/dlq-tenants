4abb15cbf0ee76932f1910eb575bae4c974f132d
Damilare Tolulope <damilaretolulope1@gmail.com>
2024-05-29 18:38:13 +0100

    chore: update next config

--- src/app/dashboard/tenants/[id]/page.tsx
+++ src/app/dashboard/tenants/[id]/page.tsx
@@ -1,17 +1,6 @@
 import { Tenant } from "@/utils/types";
 import TenantDetails from "./tenant";
-import { getCookies } from "cookies-next";
-import { cookies } from "next/headers";
 import Auth from "@/utils/auth";
-
-const getToken = () => {
-  const cookieStore = getCookies();
-  // const hCookies = cookies();
-  console.log(cookieStore);
-  const token = Auth.getToken() || "";
-
-  return token;
-};
 
 export async function generateStaticParams() {
   const res = await fetch(
@@ -20,8 +9,7 @@ export async function generateStaticParams() {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "x-api-key":
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhbWlsYXJldG9sdWxvcGUxQGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE3MTY5MzA5MTZ9.pvuaxzkrQgQgiM42jAkyWt8FNr_kOEn-gT5QTtnZCZA",
+        "x-api-key": Auth.getToken() as string,
       },
     }
   );
